3555b590255896120b824d3f594a744eee180d3c
miquellifontana <miquellirfontana@gmail.com>
2016-08-21 17:58:14 -0300

    Criado projeto java OcorrenciasAereas
    Criado projeto java no NetBeans para o projeto OcorrenciasAereas
    
    Task: http://redmine.ft.unicamp.br/redmine/issues/295

---
+++ OcorrenciasAereas/src/ocorrenciasaereas/OcorrenciasAereas.java
@@ -0,0 +1,15 @@
+package ocorrenciasaereas;
+
+/**
+ * TODO: Documentar
+ */
+public class OcorrenciasAereas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
