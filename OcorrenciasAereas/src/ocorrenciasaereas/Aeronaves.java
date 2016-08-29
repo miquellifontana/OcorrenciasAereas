098ba15f9f6e6f38ce329cbce21c621cf476fdc0
Guilherme Nunes Ferreira <guilhermen@ciandt.com>
2016-08-28 21:01:22 -0300

    Criação do arquivo Aeronaves com base no csv

---
+++ OcorrenciasAereas/src/ocorrenciasaereas/Aeronaves.java
@@ -0,0 +1,216 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ocorrenciasaereas;
+
+import java.util.Date;
+
+/**
+ *
+ * @author guilhermen
+ */
+public class Aeronaves {
+    
+    private int codigoAeronave; // 4
+    private int codigoOcorrencia ; //45602
+    private String matricula ; //PPGXE
+    private int codigoOperador ; //241
+    private String equipamento ; //AVIÃO
+    private String fabricante ; //"NEIVA INDUSTRIA AERONAUTICA"
+    private String modelo ; //56-C
+    private String tipoMotor ; //PISTÃO
+    private int quantidadeMotores ; //1
+    private int pesoMaximoDecolagem ; //660
+    private int quantidadeAssentos ; //2
+    private int anoFabricacao ; //1962
+    private String paisRegistro ; //BRASIL
+    private String categoriaRegistro ; //PRI
+    private String categoriaAviacao ; //INSTRUÇÃO
+    private String origemVoo ; // SDPW
+    private String destinoVoo ; //SDPW
+    private String faseOperacao ; //INDETERMINADA
+    private String tipoOperacao ; //INSTRUÇÃO
+    private String nivelDano ; //SUBSTANCIAL
+    private int quantidadeFatalidades ; //NULL
+    private Date dia_extracao ; //2016-07-30
+
+    public int getCodigoAeronave() {
+        return codigoAeronave;
+    }
+
+    public void setCodigoAeronave(int codigoAeronave) {
+        this.codigoAeronave = codigoAeronave;
+    }
+
+    public int getCodigoOcorrencia() {
+        return codigoOcorrencia;
+    }
+
+    public void setCodigoOcorrencia(int codigoOcorrencia) {
+        this.codigoOcorrencia = codigoOcorrencia;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public int getCodigoOperador() {
+        return codigoOperador;
+    }
+
+    public void setCodigoOperador(int codigoOperador) {
+        this.codigoOperador = codigoOperador;
+    }
+
+    public String getEquipamento() {
+        return equipamento;
+    }
+
+    public void setEquipamento(String equipamento) {
+        this.equipamento = equipamento;
+    }
+
+    public String getFabricante() {
+        return fabricante;
+    }
+
+    public void setFabricante(String fabricante) {
+        this.fabricante = fabricante;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getTipoMotor() {
+        return tipoMotor;
+    }
+
+    public void setTipoMotor(String tipoMotor) {
+        this.tipoMotor = tipoMotor;
+    }
+
+    public int getQuantidadeMotores() {
+        return quantidadeMotores;
+    }
+
+    public void setQuantidadeMotores(int quantidadeMotores) {
+        this.quantidadeMotores = quantidadeMotores;
+    }
+
+    public int getPesoMaximoDecolagem() {
+        return pesoMaximoDecolagem;
+    }
+
+    public void setPesoMaximoDecolagem(int pesoMaximoDecolagem) {
+        this.pesoMaximoDecolagem = pesoMaximoDecolagem;
+    }
+
+    public int getQuantidadeAssentos() {
+        return quantidadeAssentos;
+    }
+
+    public void setQuantidadeAssentos(int quantidadeAssentos) {
+        this.quantidadeAssentos = quantidadeAssentos;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public String getPaisRegistro() {
+        return paisRegistro;
+    }
+
+    public void setPaisRegistro(String paisRegistro) {
+        this.paisRegistro = paisRegistro;
+    }
+
+    public String getCategoriaRegistro() {
+        return categoriaRegistro;
+    }
+
+    public void setCategoriaRegistro(String categoriaRegistro) {
+        this.categoriaRegistro = categoriaRegistro;
+    }
+
+    public String getCategoriaAviacao() {
+        return categoriaAviacao;
+    }
+
+    public void setCategoriaAviacao(String categoriaAviacao) {
+        this.categoriaAviacao = categoriaAviacao;
+    }
+
+    public String getOrigemVoo() {
+        return origemVoo;
+    }
+
+    public void setOrigemVoo(String origemVoo) {
+        this.origemVoo = origemVoo;
+    }
+
+    public String getDestinoVoo() {
+        return destinoVoo;
+    }
+
+    public void setDestinoVoo(String destinoVoo) {
+        this.destinoVoo = destinoVoo;
+    }
+
+    public String getFaseOperacao() {
+        return faseOperacao;
+    }
+
+    public void setFaseOperacao(String faseOperacao) {
+        this.faseOperacao = faseOperacao;
+    }
+
+    public String getTipoOperacao() {
+        return tipoOperacao;
+    }
+
+    public void setTipoOperacao(String tipoOperacao) {
+        this.tipoOperacao = tipoOperacao;
+    }
+
+    public String getNivelDano() {
+        return nivelDano;
+    }
+
+    public void setNivelDano(String nivelDano) {
+        this.nivelDano = nivelDano;
+    }
+
+    public int getQuantidadeFatalidades() {
+        return quantidadeFatalidades;
+    }
+
+    public void setQuantidadeFatalidades(int quantidadeFatalidades) {
+        this.quantidadeFatalidades = quantidadeFatalidades;
+    }
+
+    public Date getDia_extracao() {
+        return dia_extracao;
+    }
+
+    public void setDia_extracao(Date dia_extracao) {
+        this.dia_extracao = dia_extracao;
+    }
+    
+    
+}
