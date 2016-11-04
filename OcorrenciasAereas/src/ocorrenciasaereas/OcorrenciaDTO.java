3fdb322cf0f9c1402cf08e177f501194246a650f
felipe-ferreira7 <felipe.ferreira7@hotmail.com>
2016-11-03 21:01:47 -0300

    Implementação de public Object[] atributosToArray() e Formatação de data no construtor

--- OcorrenciasAereas/src/ocorrenciasaereas/OcorrenciaDTO.java
+++ OcorrenciasAereas/src/ocorrenciasaereas/OcorrenciaDTO.java
@@ -1,6 +1,6 @@
 package ocorrenciasaereas;
 
-import java.util.Date;
+import java.text.SimpleDateFormat;
 
 /**
  * Classe que representa o modelo para exibição em tela.
@@ -13,7 +13,7 @@ public class OcorrenciaDTO {
     private String tipo;
     private String localidade;
     private String uf;
-    private Date dataOcorrencia;
+    private String dataOcorrencia;
     private Integer quantidadeFatalidades;
 
     /**
@@ -22,11 +22,14 @@ public class OcorrenciaDTO {
      * @param ocorrencia usado para obter os outros atributos :D
      */
     public OcorrenciaDTO(Ocorrencia ocorrencia) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
         this.codigoOcorrencia = ocorrencia.getCodigoOcorrencia();
         this.classificacao = ocorrencia.getClassificacao();
         this.tipo = ocorrencia.getTipo();
         this.localidade = ocorrencia.getLocalidade();
         this.uf = ocorrencia.getUf();
+        this.dataOcorrencia = dateFormat.format(ocorrencia.getDiaOcorrencia());
         this.quantidadeFatalidades = 0;
 
         for (Aeronave aeronaveEnvolvida : ocorrencia.getAeronavesEnvolvidas()) {
@@ -85,11 +88,11 @@ public class OcorrenciaDTO {
         this.uf = uf;
     }
 
-    public Date getDataOcorrencia() {
+    public String getDataOcorrencia() {
         return dataOcorrencia;
     }
 
-    public void setDataOcorrencia(Date dataOcorrencia) {
+    public void setDataOcorrencia(String dataOcorrencia) {
         this.dataOcorrencia = dataOcorrencia;
     }
 
@@ -99,5 +102,25 @@ public class OcorrenciaDTO {
 
     public void setQuantidadeFatalidades(Integer quantidadeFatalidades) {
         this.quantidadeFatalidades = quantidadeFatalidades;
+    }
+
+    /**
+     * Retorna um Arranjo de Object com os valores de cada atributo do objeto.
+     *
+     * @return Object[] onde cada posição representa o valor de um atributo da
+     * classe
+     */
+    public Object[] atributosToArray() {
+        Object[] campos = new Object[7];
+
+        campos[0] = this.codigoOcorrencia;
+        campos[1] = this.classificacao;
+        campos[2] = this.tipo;
+        campos[3] = this.localidade;
+        campos[4] = this.uf;
+        campos[5] = this.dataOcorrencia;
+        campos[6] = this.quantidadeFatalidades;
+
+        return campos;
     }
 }
